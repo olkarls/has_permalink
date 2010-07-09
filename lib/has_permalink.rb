6f72e30c39dce51933eca64d11b67b5fe0aa4cc4
Ola Karlsson <olkarls@gmail.com>
2010-07-09 15:52:15 +0200

    fixed configuration

--- lib/has_permalink.rb
+++ lib/has_permalink.rb
@@ -7,20 +7,16 @@ module HasPermalink
       class_inheritable_accessor :options
       include Behavior
     end
-    
-    #validates_presence_of options[:field_to_generate_from].to_symbol, options[:field_to_generate_from].to_symbol
-    #validates_uniqueness_of options[:field_to_generate_from].to_symbol, :case_insensitive => true
-    
-    validates_presence_of :title, :permalink
-    validates_uniqueness_of :permalink, :case_insensitive => true               
-    before_validation :generate_permalink    
+
+    self.options = options
+    before_validation :generate_permalink
   end
   
   module Behavior
-    include FriendlyUrl    
+    include FriendlyUrl
     def generate_permalink
       if @permalink.blank?
-        self.permalink = normalize(self.title)
+        eval("self.#{options[:field_to_generate]}") = normalize(eval("self.#{options[:field_to_generate_from]}"))
       end
     end
     
