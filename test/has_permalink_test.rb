04f78ed3dd3ddebd48a8ebf925f00b0616cee221
Ola Karlsson <olkarls@gmail.com>
2011-01-31 20:54:48 +0100

    Added a test for foreign charachters.

--- test/has_permalink_test.rb
+++ test/has_permalink_test.rb
@@ -4,49 +4,49 @@ require 'test_helper'
 
 class HasPermalinkTest < Test::Unit::TestCase
   load_schema
-  
+
   class Post < ActiveRecord::Base
     has_permalink
   end
-  
+
   class Category < ActiveRecord::Base
     has_permalink(:name)
   end
-  
+
   class User < ActiveRecord::Base
     has_permalink(:name)
   end
-  
+
   def test_should_generate_permalink
     post = Post.new(:title => "En annorlunda titel med åäö")
     post.valid?
     assert_equal "en-annorlunda-titel-med-aao", post.permalink
   end
-  
+
   def test_should_not_edit_attribute_if_permalink_is_supplied
     post = Post.new(:title => "En annorlunda titel med åäö", :permalink => "first-post")
     post.valid?
     assert_equal "first-post", post.permalink
   end
-  
+
   def test_should_generate_permalink_from_other_attribute_then_title
     category = Category.new(:name => "some really cool category")
     category.valid?
     assert_equal "some-really-cool-category", category.permalink
   end
-  
+
   def test_should_not_generate_more_than_one_dash
     post = Post.new(:title => "Home - some cool site - some slogan")
     post.valid?
     assert_equal "home-some-cool-site-some-slogan", post.permalink
   end
-  
+
   def test_should_generate_without_callback
     post = Post.new(:title => "Really teasing title")
     post.generate_permalink
     assert_equal "really-teasing-title", post.permalink
   end
-  
+
   def test_force_regeneration
     post = Post.new(:title => "Really teasing title")
     post.valid?
@@ -58,5 +58,13 @@ class HasPermalinkTest < Test::Unit::TestCase
     assert_equal "really-teasing-title", post.permalink
     post.generate_permalink!
     assert_equal "another-title", post.permalink
+  end
+
+  def test_it_generates_useful_permalink_from_nonsense
+    crazy_title      = '< /\|/\ > - SÖme REÄLLY CrÅzÛ stÖffzzz!!!! <<<<=======, CÖöL!'
+    useful_permalink = 'some-really-crazu-stoffzzz-cool'
+    post = Post.new(:title => crazy_title)
+    post.valid?
+    assert_equal useful_permalink, post.permalink
   end
 end
