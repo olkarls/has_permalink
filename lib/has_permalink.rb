2f70d7a70bf01d9df1e1354e225e058ef610e2cc
Ola Karlsson <olkarls@gmail.com>
2011-02-01 21:21:44 +0100

    Changed the structure and added a class method for generating permalinks to all instances.

--- lib/has_permalink.rb
+++ lib/has_permalink.rb
@@ -1,26 +1,38 @@
 require 'friendly_url'
 
 module HasPermalink
+  require 'railtie' if defined?(Rails)
+
   def has_permalink(generate_from = :title)
-    unless included_modules.include? Behavior
+    unless included_modules.include? InstanceMethods
       class_inheritable_accessor :generate_from
-      include Behavior
+      extend ClassMethods
+      include InstanceMethods
     end
 
     self.generate_from = generate_from
     before_validation :generate_permalink
   end
-  
-  module Behavior
+
+  module ClassMethods
+    def generate_permalinks
+      self.all.each do |item|
+        item.generate_permalink
+        item.save
+      end
+    end
+  end
+
+  module InstanceMethods
     include FriendlyUrl
     def generate_permalink
       self.permalink = normalize(self.send(generate_from)) if permalink.blank?
     end
-    
+
     def generate_permalink!
       self.permalink = normalize(self.send(generate_from))
     end
-    
+
     def to_param
       permalink
     end
