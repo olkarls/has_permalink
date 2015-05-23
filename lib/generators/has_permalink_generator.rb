1a7adf168d8fa35e1b4af0883ca2113de3983c6d
Ola Karlsson <olkarls@gmail.com>
2015-05-23 14:29:41 +0200

    Make some methods public for Rails 4 support.

--- lib/generators/has_permalink_generator.rb
+++ lib/generators/has_permalink_generator.rb
@@ -2,16 +2,14 @@ require 'rails/generators/active_record'
 
 class HasPermalinkGenerator < ActiveRecord::Generators::Base
   desc "Create a migration to add a permalink field to your model."
-  
+
   def self.source_root
     @source_root ||= File.expand_path('../templates', __FILE__)
   end
-  
+
   def generate_migration
     migration_template "has_permalink_migration.rb.erb", "db/migrate/#{migration_file_name}"
   end
-  
-  protected
 
   def migration_name
     "add_permalink_to_#{name.underscore}"
