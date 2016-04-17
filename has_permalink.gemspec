bcd155745203d1acdb386c91578863fe3cc78cc8
Ola Karlsson <olkarls@gmail.com>
2016-04-17 21:40:16 +0200

    Refactoring

--- has_permalink.gemspec
+++ has_permalink.gemspec
@@ -6,6 +6,7 @@ Gem::Specification.new do |gem|
   gem.summary = 'SEO Permalink Plugin for Ruby on Rails.'
   gem.files = Dir['lib/**/*', 'rails/**/*']
   gem.homepage = 'http://haspermalink.org'
+  gem.licenses = ['MIT']
   gem.add_runtime_dependency 'activerecord', '~> 4.0', '>= 2.0.0'
   gem.add_development_dependency 'simplecov', '~> 0.11', '>= 0.11.2'
   gem.add_development_dependency 'coveralls', '~> 0.8.13', '>= 0.8.13'
--- lib/friendly_url.rb
+++ lib/friendly_url.rb
@@ -1,37 +1,46 @@
 # encoding: utf-8
 module FriendlyUrl
   $KCODE = "U" unless RUBY_VERSION > "1.9.0"
+
+  def replace_patterns
+    [
+      { pattern: /[àáâãäåāă]/,     replace_with: 'a'},
+      { pattern: /æ/,              replace_with: 'ae'},
+      { pattern: /[ďđ]/,           replace_with: 'd'},
+      { pattern: /[çćčĉċ]/,        replace_with: 'c'},
+      { pattern: /[èéêëēęěĕė]/,    replace_with: 'e'},
+      { pattern: /ƒ/,              replace_with: 'f'},
+      { pattern: /[ĝğġģ]/,         replace_with: 'g'},
+      { pattern: /[ĥħ]/,           replace_with: 'h'},
+      { pattern: /[ììíîïīĩĭ]/,     replace_with: 'i'},
+      { pattern: /[įıĳĵ]/,         replace_with: 'j'},
+      { pattern: /[ķĸ]/,           replace_with: 'k'},
+      { pattern: /[łľĺļŀ]/,        replace_with: 'l'},
+      { pattern: /[ñńňņŉŋ]/,       replace_with: 'n'},
+      { pattern: /[òóôõöøōőŏŏ]/,   replace_with: 'o'},
+      { pattern: /œ/,              replace_with: 'oe'},
+      { pattern: /ą/,              replace_with: 'q'},
+      { pattern: /[ŕřŗ]/,          replace_with: 'r'},
+      { pattern: /[śšşŝș]/,        replace_with: 's'},
+      { pattern: /[ťţŧț]/,         replace_with: 't'},
+      { pattern: /[ùúûüūůűŭũų]/,   replace_with: 'u'},
+      { pattern: /ŵ/,              replace_with: 'w'},
+      { pattern: /[ýÿŷ]/,          replace_with: 'y'},
+      { pattern: /[žżź]/,          replace_with: 'z'},
+      { pattern: /\s+/,            replace_with: '-'},
+      { pattern: /[^\sa-z0-9_-]/,  replace_with: ''},
+      { pattern: /-{2,}/,          replace_with: '-'},
+      { pattern: /^-/,             replace_with: ''},
+      { pattern: /-$/,             replace_with: ''}
+    ]
+  end
+
   def normalize(str)
     unless str.blank?
       n = str.mb_chars.downcase.to_s.strip
-      n.gsub!(/[àáâãäåāă]/,     'a')
-      n.gsub!(/æ/,              'ae')
-      n.gsub!(/[ďđ]/,           'd')
-      n.gsub!(/[çćčĉċ]/,        'c')
-      n.gsub!(/[èéêëēęěĕė]/,    'e')
-      n.gsub!(/ƒ/,              'f')
-      n.gsub!(/[ĝğġģ]/,         'g')
-      n.gsub!(/[ĥħ]/,           'h')
-      n.gsub!(/[ììíîïīĩĭ]/,     'i')
-      n.gsub!(/[įıĳĵ]/,         'j')
-      n.gsub!(/[ķĸ]/,           'k')
-      n.gsub!(/[łľĺļŀ]/,        'l')
-      n.gsub!(/[ñńňņŉŋ]/,       'n')
-      n.gsub!(/[òóôõöøōőŏŏ]/,   'o')
-      n.gsub!(/œ/,              'oe')
-      n.gsub!(/ą/,              'q')
-      n.gsub!(/[ŕřŗ]/,          'r')
-      n.gsub!(/[śšşŝș]/,        's')
-      n.gsub!(/[ťţŧț]/,         't')
-      n.gsub!(/[ùúûüūůűŭũų]/,   'u')
-      n.gsub!(/ŵ/,              'w')
-      n.gsub!(/[ýÿŷ]/,          'y')
-      n.gsub!(/[žżź]/,          'z')
-      n.gsub!(/\s+/,            '-')
-      n.gsub!(/[^\sa-z0-9_-]/,  '')
-      n.gsub!(/-{2,}/,          '-')
-      n.gsub!(/^-/,             '')
-      n.gsub!(/-$/,             '')
+      replace_patterns.each do |rule|
+        n.gsub!(rule[:pattern], rule[:replace_with])
+      end
       n
     end
   end
